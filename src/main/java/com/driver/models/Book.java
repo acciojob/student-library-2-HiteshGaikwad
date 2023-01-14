16315fa747fdaca9b82199529d15efe1651b0df5
Hitesh <gaikwadhitesh03@gmail.com>
2023-01-14 12:11:27 +0530

    con

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -43,5 +43,12 @@ public class Book {
 
     public Book() {
     }
+
+    public Book(String name, Genre genre, Author author) {
+
+        this.name = name;
+        this.genre = genre;
+        this.author = author;
+    }
 }
 
