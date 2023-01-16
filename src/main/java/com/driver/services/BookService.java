4b8dc8e32aae4de44b0644e07b643de24ddab378
Hitesh <gaikwadhitesh03@gmail.com>
2023-01-16 22:03:06 +0530

    a

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -48,7 +48,7 @@ public class BookService {
     public List<Book> getBooks(String genre, boolean available, String author){
 
         //find the elements of the list by yourself
-        List<Book> books =null;
+        List<Book> books =new ArrayList<>();
         if(genre!=null && author!=null) {
             books=bookRepository2.findBooksByGenreAuthor(genre, author, available);
         }else if(genre==null && author!=null){
